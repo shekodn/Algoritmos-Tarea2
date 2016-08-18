e6774ac5c2e874544a22a2edc0041fa2615ae6ca
shekodn <sdiazn19@gmail.com>
2016-08-17 22:23:59 -0500

    hash function

--- tarea2_a01192313.cpp
+++ tarea2_a01192313.cpp
@@ -3,10 +3,17 @@ using namespace std;
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <string>     // std::string, std::stoi
+
 
 
 //Sergio Diaz
 //A01192313
+
+
+//Globales
+string sArchivo = "matriculas.txt";
+
 
 // cd sdn/TEC/V/Algoritmos/Tareas/Tarea_2/Algoritmos-Tarea2/
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -23,58 +30,45 @@ int arr1[154750];
 void leeArchivo1(){
 
 	ifstream matriculas;
-	matriculas.open("matriculas.txt");
+	matriculas.open(sArchivo);
 	int iNumero;
 	int iPosicion = 0;
 
 	while (matriculas >> iNumero) {
-
-		cout << iNumero << endl;
 		arr1[iPosicion] = iNumero;
 		iPosicion++;
-
 	}
 
-	//matriculas.close();
-	iPosicion = 0;
+	matriculas.close();
 }
 
 void buscarArreglo(int iDato){
 
 	bool find = false;
-	int iPosicion = 0;
 
 	for (int i = 0; i < iSize; i++) {
-
-		if  (arr1[iPosicion] == iDato) {
-
+		if  (arr1[i] == iDato) {
 			find = true;
 			break;
 		}
-
-		iPosicion++;
 	}
 
 	if (find) {
-
 		cout << "Dato encontrado" << endl;
-
 	}
 
 	else {
-
-		cout << "Dato no encontrado" << endl;
+		cout << "Dato NO encontrado" << endl;
 	}
-
 }
 
 
 
-void solucion1(){
+void solucion1(int iDato){
 
 	cout << "Solucion #1" << endl;
 	leeArchivo1();
-	buscarArreglo(4855469);
+	buscarArreglo(iDato);
 
 
 }
@@ -89,14 +83,11 @@ vector<int> v;  //vector donde se colocan las maticulas
 void leeArchivo2(){
 
 	ifstream matriculas;
-	matriculas.open("mock.txt");
+	matriculas.open(sArchivo);
 	int iNumero;
 
 	while (matriculas >> iNumero) {
-
-		cout << iNumero << endl;
 		v.push_back(iNumero);
-
 	}
 
 	//Se ordena el vector en forma ascendiente 
@@ -108,48 +99,103 @@ void leeArchivo2(){
   	for (vector<int>::iterator it=v.begin(); it!=v.end(); ++it)
     cout << * it << endl;
   	cout << endl;
-  	*/
+  	*/	
 
+  	matriculas.close();
 
-
-  	
 }
 
 void bSearch2(int iDato){
 
 	//binary search
   	if (binary_search(v.begin(), v.end(), iDato)) {
-    	cout << "Found " << iDato << endl;
+
+  		cout << "Dato encontrado" << endl;
     } 
     
     else {
-        
-    	cout << "NOT found" << endl;
+       
+    	cout << "Dato NO encontrado" << endl;
     }
 }
 
 
-void solucion2(){
+void solucion2(int iDato){
 
 	leeArchivo2();
-	bSearch2(485546911);
+	bSearch2(iDato);
 
 }
 
-void solucion3(){
+////////////////////////////////////////////////////////////////////////////////////////////////
+//Solucion #2
+
+int arrHash[1000]; //arreglo estático de 1000 posiciones
+
+void solucion3(int iDato){
+
+	
 
 
 }
+
+//Le archivo de texto y mete en el arreglo a las matriculas
+void leeArchivo3(){
+
+	ifstream matriculas;
+	matriculas.open(sArchivo);
+	string sNumero;
+	
+	while (matriculas >> sNumero) {
+	}
+
+	matriculas.close();
+}
+
+
+
+int funcionHashing(string sNumero){
+
+	int iNumero;
+
+	//checar los ultimos 3 digitos
+	sNumero.erase (sNumero.begin(), sNumero.end()-3);
+
+	iNumero = stoi(sNumero);
+
+	return iNumero;
+}
+
+
+
 
 int main(int argc, char const *argv[]) {
 	
 
-	//Solucion 1
-	//solucion1();
-	
-	//Solucion 2
-	solucion2();
+	/*
+	int iDato = 2692879;
 
-	//Solucion 3
-	solucion3();
+	cout << "Inserte la matricula que quiere buscar, si quiere salir ponga -1" << endl;
+	cin >> iDato;
+
+
+	while (iDato != -1) {
+
+		//Solucion 1
+		solucion1(iDato);
+		
+		//Solucion 2
+		//solucion2(iDato);
+
+		//Solucion 3
+		solucion3(iDato);
+
+		cout << "Si quiere insertar un dato ponga 1, si quiere salir ponga 0" << endl;
+		cin >> iDato;
+
+	}
+	*/
+
+	cout << funcionHashing("1192313") << endl;
+
 }
